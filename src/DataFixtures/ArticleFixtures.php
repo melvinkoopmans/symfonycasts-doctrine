709692e76be7556b6e4748813f95ff3183ad5fb5
Melvin Koopmans <melvin.koopmans@gmail.com>
2019-12-07 17:19:01 +0100

    show article tags on home and detail

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,9 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Tag;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
+class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleImages = [
         'asteroid.jpeg',
@@ -27,6 +29,22 @@ class ArticleFixtures extends BaseFixture
             $article->setAuthor($this->faker->name)
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
+
+            /** @var Tag[] $tags */
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+            }
         });
+    }
+
+    /**
+     * @return array
+     */
+    public function getDependencies(): ?array
+    {
+        return [
+            TagFixture::class
+        ];
     }
 }
